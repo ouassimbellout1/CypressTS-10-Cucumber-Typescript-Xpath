f5378d2c0f7b3ae91e1b4291135dad4f2ccacd7e
ouassim bellout <ouassim.bellout.1@gmail.com>
2022-10-03 18:15:38 -0400

    cypress 10 v7

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -12,9 +12,15 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands'
+// Import commands.js using ES2015 syntax:
 require('cypress-xpath')
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+declare global {
+    namespace Cypress {
+        interface Chainable <Subject = any> {
+            googleSearch(): void;
+        }
+    }
+}
